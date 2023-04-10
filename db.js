f65bcfd1d13cd2876e44b0bdd0b13c50b5a73485
Armanidrisi <idrisiarman17@gmail.com>
2023-04-10 14:39:02 +0530

    Added Database Connection

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectToMongo = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Arman:Arman@cluster0.kmyozhq.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://Arman:Arman@cluster0.kmyozhq.mongodb.net/?retryWrites=true&w=majority");
       console.log("connectToMongo Success")
-    );
+    ;
   } catch (e) {
     console.log("Failed To Connect To MongoDB " + e);
   }
