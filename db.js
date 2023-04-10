06a3e486fa3588474c1e05f09f8223a5f7d2d626
Armanidrisi <idrisiarman17@gmail.com>
2023-04-10 15:41:23 +0530

    Database Details Changed In db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToMongo = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Arman:Arman@cluster0.kmyozhq.mongodb.net/?retryWrites=true&w=majority");
+      "mongodb+srv://youruser:yourpass@yoircluster.yourhost.mongodb.net/?retryWrites=true&w=majority");
       console.log("connectToMongo Success")
     ;
   } catch (e) {
